87718ba93ff5d86d0237def0a9e2300272e096c5
Lambda_zb <zhongbo19940130@gmail.com-local>
2024-12-06 23:46:04 +0800

    Add project files.

---
+++ WpfLabelAttribute/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLabelAttribute
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            // FindResource return type is object
+            if (this.FindResource("human") is Human h)
+            {
+                MessageBox.Show(h.Child?.Name);
+            }
+            
+        }
+    }
+
+    [TypeConverter(typeof(NameToHumanTypeConverter))]
+    public class Human
+    {
+        public string Name { get; set; }
+        public Human Child { get; set; }
+    }
+
+    // convert String value to Human type vale
+    public class NameToHumanTypeConverter : TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, 
+            CultureInfo culture, object value)
+        {
+            var name = value?.ToString();
+            var human = new Human
+            {
+                Name = name
+            };
+            return human;
+        }
+    }
+}
