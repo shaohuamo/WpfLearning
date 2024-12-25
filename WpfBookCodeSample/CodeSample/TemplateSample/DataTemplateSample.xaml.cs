2e39046acc640e860d80dac9aeb18f9c525ab7c5
Lambda_zb <zhongbo19940130@gmail.com-local>
2024-12-25 22:18:28 +0800

    Add Template Code Sample

---
+++ WpfBookCodeSample/CodeSample/TemplateSample/DataTemplateSample.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfBookCodeSample.Model;
+
+namespace WpfBookCodeSample.CodeSample.TemplateSample
+{
+    /// <summary>
+    /// Interaction logic for DataTemplateSample.xaml
+    /// </summary>
+    public partial class DataTemplateSample : Window
+    {
+        public DataTemplateSample()
+        {
+            InitializeComponent();
+            InitialCarList();
+        }
+
+        private void InitialCarList()
+        {
+            List<Car> carList = new List<Car>() {
+                new Car(){ Automaker="Lamborghini", Name="Diablo", TopSpeed="200", Year="1990"},
+                new Car(){ Automaker="Lamborghini", Name="Murcielago", TopSpeed="250", Year="1998"},
+                new Car(){ Automaker="Lamborghini", Name="Gallardo", TopSpeed="300", Year="2002"},
+                new Car(){ Automaker="Lamborghini", Name="Reventon", TopSpeed="350", Year="2011"},
+            };
+            //填充数据源
+            listBoxCars.ItemsSource = carList;
+        }
+    }
+}
---
+++ WpfBookCodeSample/Model/NameToPhotoPathConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfBookCodeSample.Model
+{
+    /// <summary>
+    /// 汽车名称转换为照片路径----Used in DataTemplateSample.xaml
+    /// </summary>
+    public class NameToPhotoPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string uriStr = $@"../../Resources/Images/{(string)value}.jpg";
+            return new BitmapImage(new Uri(uriStr, UriKind.Relative));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
