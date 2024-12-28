ea484f794dcc1d59a6d866be3779437e849e01d3
Lambda_zb <zhongbo19940130@gmail.com-local>
2024-12-28 15:51:56 +0800

    Add Trigger Code Sample

---
+++ WpfBookCodeSample/CodeSample/StyleSample/Trigger/MultiDataTriggerSample.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfBookCodeSample.Model;
+
+namespace WpfBookCodeSample.CodeSample.StyleSample.Trigger
+{
+    /// <summary>
+    /// Interaction logic for MultiDataTriggerSample.xaml
+    /// </summary>
+    public partial class MultiDataTriggerSample : Window
+    {
+        public MultiDataTriggerSample()
+        {
+            InitializeComponent();
+            InitialInfo();
+        }
+
+        private void InitialInfo()
+        {
+            List<Person> infos = new List<Person>() {
+                new Person(){ Id=2, Name="Darren", Skill="WPF"},
+                new Person(){ Id=1, Name="Tom", Skill="Java"},
+                new Person(){ Id=3, Name="Jacky", Skill="Asp.net"},
+                new Person(){ Id=2, Name="Andy", Skill="C#"},
+            };
+            listBoxPerson.ItemsSource = infos;
+        }
+    }
+}
---
+++ WpfBookCodeSample/Model/L2BConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfBookCodeSample.Model
+{
+    /// <summary>
+    /// used in DataTriggerSample.xaml
+    /// </summary>
+    public class L2BConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                int textLength = (int)value;
+                return textLength > 6;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
